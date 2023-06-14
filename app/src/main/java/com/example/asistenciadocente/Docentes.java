02e1c067be32e450a082529bff4e78bb07be7d45
CruzRubio <cruz.a.rubio7@gmail.com>
2023-06-14 14:45:06 -0700

    Inicio de sesion pantalla

---
+++ app/src/main/java/com/example/asistenciadocente/Docentes.java
@@ -0,0 +1,14 @@
+package com.example.asistenciadocente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Docentes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_docentes);
+    }
+}
--- app/src/main/java/com/example/asistenciadocente/MainActivity.java
+++ app/src/main/java/com/example/asistenciadocente/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.asistenciadocente;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextInputEditText usuario,contraseña;
+    Button ingresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
