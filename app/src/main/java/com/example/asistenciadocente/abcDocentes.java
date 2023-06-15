129a798b49530f4d4d9151d5dc189991159c0147
DELLAlfredo <loboluisalfredo@hotmail.com>
2023-06-14 20:56:23 -0700

    saa

---
+++ app/src/main/java/com/example/asistenciadocente/abcDocentes.java
@@ -0,0 +1,14 @@
+package com.example.asistenciadocente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class abcDocentes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abc_docentes);
+    }
+}
