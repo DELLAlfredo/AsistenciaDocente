2d049ed53df81bc1828c9e7cce8486e38e6a1d94
CruzRubio <cruz.a.rubio7@gmail.com>
2023-06-27 22:12:15 -0700

    reporte fecha

--- app/src/main/java/com/example/asistenciadocente/Reporte.java
+++ app/src/main/java/com/example/asistenciadocente/Reporte.java
@@ -37,7 +37,7 @@ public class Reporte extends menu {
     ImageButton Btncalendario;
     DatePicker dpfecha;
     TableLayout tabla;
-    Button btnbusquedafiltrada;
+    Button btnbusquedafiltrada,btnbusquedafiltradaporfecha;
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -48,18 +48,19 @@ public class Reporte extends menu {
         setContentView(activityReporteBinding.getRoot());
         allocateActivityTitle("Reporte");
 
+
         sphora = findViewById(R.id.sphora);
-        String[] hora = {"7AM-8AM","8AM-9AM","9AM-10AM","10AM-11AM","11AM-12AM","12AM-1PM","1PM-2PM","2PM-3PM"};
+        String[] hora = {"7AM-8AM", "8AM-9AM", "9AM-10AM", "10AM-11AM", "11AM-12AM", "12AM-1PM", "1PM-2PM", "2PM-3PM"};
         ArrayAdapter<String> Adapterhora = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, hora);
         sphora.setAdapter(Adapterhora);
 
         spopcion = findViewById(R.id.spopcion);
-        String[] opcion = {"IMPARTIDA","NO IMPARTIDA","CLASE INCOMPLETA","SUSPENCION"};
+        String[] opcion = {"IMPARTIDA", "NO IMPARTIDA", "CLASE INCOMPLETA", "SUSPENCION"};
         ArrayAdapter<String> Adapteropcion = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opcion);
         spopcion.setAdapter(Adapteropcion);
 
-        txtfecha =findViewById(R.id.etfecha);
-        dpfecha =findViewById(R.id.dtfecha);
+        txtfecha = findViewById(R.id.etfecha);
+        dpfecha = findViewById(R.id.dtfecha);
 
         txtfecha.setText(getfecha());
         dpfecha.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
@@ -71,8 +72,7 @@ public class Reporte extends menu {
             }
         });
 
-
-        TableLayout tabla = findViewById(R.id.tabla);
+        tabla = findViewById(R.id.tabla);
 
         // Realizar una solicitud a la API para obtener los datos
         String url = "http://192.168.56.1:80/checador/reporteselect.php"; // Reemplaza con la URL de tu API
@@ -119,7 +119,6 @@ public class Reporte extends menu {
                                 TextView txtfecha = new TextView(Reporte.this);
                                 txtfecha.setText(fecha);
 
-
                                 // Agregar los TextViews a la fila
                                 fila.addView(txtdocentes);
                                 fila.addView(txtaula);
@@ -141,24 +140,32 @@ public class Reporte extends menu {
             }
         });
 
-// Agregar la solicitud a la cola
+        // Agregar la solicitud a la cola
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(jsonObjectRequest);
-        //boton de filtro
-        btnbusquedafiltrada= findViewById(R.id.BtnBusqueda_filtrada);
 
-        String fecha = txtfecha.getText().toString();
-        String horas = sphora.getSelectedItem().toString();
-        String opciones = spopcion.getSelectedItem().toString();
+        // Botón de filtro
+        btnbusquedafiltrada = findViewById(R.id.BtnBusqueda_filtrada);
 
         btnbusquedafiltrada.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String fecha = txtfecha.getText().toString();
+                String horas = sphora.getSelectedItem().toString();
+                String opciones = spopcion.getSelectedItem().toString();
                 llenaTablaConAPIFiltrada(fecha, horas, opciones);
-
             }
         });
 
+        //
+        btnbusquedafiltradaporfecha =findViewById(R.id.btnFiltradofecha);
+        btnbusquedafiltradaporfecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fecha = txtfecha.getText().toString();
+                llenaTablaConAPIFiltradaporfehca(fecha);
+            }
+        });
 
     }
     public String getfecha(){
@@ -189,6 +196,84 @@ public class Reporte extends menu {
 
     private void llenaTablaConAPIFiltrada(String fecha, String hora, String opcion) {
         String url = "http://192.168.56.1:80/checador/reportefiltrado.php?fecha=" + fecha + "&hora=" + hora + "&opcion=" + opcion; // Reemplaza con la URL de tu API
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Limpiar la tabla antes de agregar los nuevos datos
+                        tabla.removeAllViews();
+
+                        try {
+                            // Crear la fila de encabezados
+                            TableRow headerRow = new TableRow(Reporte.this);
+                            String[] headers = {"docentes", "aula", "hora","opcion","fecha"};
+                            for (String header : headers) {
+                                TextView textView = new TextView(Reporte.this);
+                                textView.setText(header);
+                                textView.setPadding(5, 5, 5, 5);
+                                textView.setBackgroundResource(R.color.purple_200);
+                                textView.setTypeface(null, Typeface.BOLD);
+                                headerRow.addView(textView);
+                            }
+                            tabla.addView(headerRow);
+
+                            // Agregar las filas con los datos filtrados
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject rowData = response.getJSONObject(i);
+                                TableRow dataRow = new TableRow(Reporte.this);
+
+                                // Obtener los valores de las columnas de la respuesta JSON
+                                String docentes = rowData.getString("docentes");
+                                String aula = rowData.getString("aula");
+                                String hora = rowData.getString("hora");
+                                String opcion = rowData.getString("opcion");
+                                String fecha = rowData.getString("fecha");
+                                // Crear los TextViews para mostrar los datos en la fila
+                                TextView txtdocentes = new TextView(Reporte.this);
+                                txtdocentes.setText(docentes);
+
+                                TextView txtaula = new TextView(Reporte.this);
+                                txtaula.setText(aula);
+
+                                TextView txthora = new TextView(Reporte.this);
+                                txthora.setText(hora);
+
+                                TextView txtopcion = new TextView(Reporte.this);
+                                txtopcion.setText(opcion);
+
+                                TextView txtfecha = new TextView(Reporte.this);
+                                txtfecha.setText(fecha);
+
+                                // Agregar los TextViews a la fila
+                                dataRow.addView(txtdocentes);
+                                dataRow.addView(txtaula);
+                                dataRow.addView(txthora);
+                                dataRow.addView(txtopcion);
+                                dataRow.addView(txtfecha);
+
+                                // Agregar la fila a la tabla
+                                tabla.addView(dataRow);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        // Agregar la solicitud a la cola
+        queue.add(jsonArrayRequest);
+    }
+
+    private void llenaTablaConAPIFiltradaporfehca(String fecha) {
+        String url = "http://192.168.56.1/checador/busquedafiltadofecha.php?fecha=" + fecha ; // Reemplaza con la URL de tu API
 
         RequestQueue queue = Volley.newRequestQueue(this);
 
